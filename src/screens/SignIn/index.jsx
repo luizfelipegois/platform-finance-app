e72e75f73fe8b2ee9cde9722148a9de833e30068
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-11-20 19:10:43 -0500

    feat: Base construction of the APP screens

---
+++ src/screens/SignIn/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Container } from './styled';
+
+export default function SignIn() {
+  return (
+    <Container>
+    </Container>
+  )
+}
---
+++ src/screens/index.jsx
@@ -0,0 +1,11 @@
+import Home from './Home';
+import News from './News';
+import Profile from './Profile';
+import SignIn from './SignIn';
+
+export default screens = {
+  Home,
+  News,
+  Profile,
+  SignIn
+};
