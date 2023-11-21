54ad4c735552941a3947a9e524b37f96147e6976
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-11-20 19:11:25 -0500

    feat: Layer management

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import THEME from './src/theme';
+import { Provider } from './src/context';
+import { Router } from './src/routes/Router';
 
 export default function App() {
+
   return (
-    <View>
-    </View>
+    <ThemeProvider theme={THEME}>
+      <Provider>
+      <StatusBar
+        animated={true}
+        hidden={false}
+        barStyle="light-content"
+      />
+      <Router />
+    </Provider>
+    </ThemeProvider>
   );
 }
