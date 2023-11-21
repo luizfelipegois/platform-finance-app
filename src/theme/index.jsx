e55c8f5e54bd660c1834c70b7bc12fb97f8c459c
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-11-20 19:09:35 -0500

    feat: APP Theme Tokens

---
+++ src/theme/index.jsx
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#050505',
+    CARDS: '#101010', 
+    SECUNDARY: '#505050',
+    TEXT: '#f2f2f2',
+    ALERT: '#ED4747',
+    ACTION: '#293DEA',
+  },
+
+  FONTS: {
+    REGULAR: 'Roboto_400Regular',
+    MEDIUM: 'Roboto_500Medium',
+    BOLD: 'Roboto_700Bold',
+  },
+
+  SIZES: {
+    TITLE: '26px',
+    SUBTITLE: '22px',
+    TEXT: '16px',
+  }
+}
