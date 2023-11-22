499542134fd45e6ff9ab73dd676de6f06313edd3
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-11-21 21:00:36 -0500

    feat: Refresh mechanism to update user data

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Container } from "./Styled";
+import { RefreshControl, ScrollView } from "react-native";
+import { Context } from '../../context';
 
 export default function Home() {
+  const { loading, getUserData } = useContext(Context);
+
   return (
-    <Container></Container>
+    <Container>
+     <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={loading} onRefresh={() => getUserData()} tintColor="#f2f2f2" colors="#f2f2f2" />
+      }
+     >
+
+     </ScrollView>
+    </Container>
   )
 }
