d6837e1f4d86f4d0c41b335621588d54e2252b59
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-11-22 20:20:07 -0500

    feat: Creating settings screens

---
+++ src/screens/ChangePassword/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Styled";
+
+export default function ChangePassword() {
+  return (
+    <Container></Container>
+  )
+}
---
+++ src/screens/Requests/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./Styled";
+
+export default function Requests() {
+  return (
+    <Container></Container>
+  )
+}
