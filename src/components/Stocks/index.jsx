e0e3bf3bba7271d1e9c3a2f0a86d18f88ae5d800
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2023-12-06 22:34:19 -0500

    feat: graph created

--- src/components/Stocks/index.jsx
+++ src/components/Stocks/index.jsx
@@ -2,9 +2,43 @@ import React from "react";
 import { Dimensions, View } from "react-native";
 import THEME from "../../theme";
 import Text from "../Text";
+import { LineChart } from "react-native-chart-kit";
+
+const CandlestickChart = () => {
+  const generateRandomData = () => {
+    return Array.from({ length: 5 }, () => Math.floor(Math.random() * 101) - 50);
+  };
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        data: generateRandomData(),
+      },
+    ],
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", marginTop: 10}}>
+      <LineChart
+        data={data}
+        width={Dimensions.get("window").width - 35}
+        height={100}
+        withShadow={false}
+        withInnerLines={false}
+        withOuterLines={false}
+        chartConfig={{
+          backgroundGradientFrom: THEME.COLORS.BLACK,
+          backgroundGradientTo: THEME.COLORS.BLACK,
+          color: () => `rgba(26, 255, 146, 1)`,
+          labelColor: () => `rgba(255, 255, 255, 0)`,
+        }}
+      />
+    </View>
+  );
+};
 
 export default function Stocks({ item, type }) {
-  console.log(item)
   return (
     <View
       style={{
@@ -12,11 +46,8 @@ export default function Stocks({ item, type }) {
           type === "short"
             ? Dimensions.get("window").width * 0.8
             : Dimensions.get("window").width - 30,
-        marginHorizontal: 10,
+        marginHorizontal: type === "short" ? 10 : 0,
         borderRadius: 12,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-start",
         borderWidth: 1,
         borderColor: THEME.COLORS.BLACK_LIGHT,
         padding: 10,
@@ -24,29 +55,36 @@ export default function Stocks({ item, type }) {
     >
       <View
         style={{
-          width: "70%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "flex-start",
         }}
       >
-        <Text text={item.companyName} fontWeight={700} />
-        <Text text={item.symbol} color={THEME.COLORS.GRAY} marginTop={10} />
+        <View
+          style={{
+            width: "70%",
+          }}
+        >
+          <Text text={item.companyName} fontWeight={700} />
+          <Text text={item.symbol} color={THEME.COLORS.GRAY} marginTop={10} />
+        </View>
+        <View
+          style={{
+            alignItems: "flex-end",
+          }}
+        >
+          <Text text={`$${item.close || item.iexAskPrice}`} fontWeight={500} />
+          <Text
+            text={`${item.change}%`}
+            color={
+              parseFloat(item.change) < 0
+                ? THEME.COLORS.RED
+                : THEME.COLORS.GREEN
+            }
+          />
+        </View>
       </View>
-      <View
-        style={{
-          alignItems: "flex-end",
-        }}
-      >
-        <Text
-          text={`$${item.close || item.iexAskPrice}`}
-          fontWeight={500}
-          marginTop={10}
-        />
-        <Text
-          text={`${item.change}%`}
-          color={
-            parseFloat(item.change) < 0 ? THEME.COLORS.RED : THEME.COLORS.GREEN
-          }
-        />
-      </View>
+      {type !== "short" && <CandlestickChart />}
     </View>
   );
 }
